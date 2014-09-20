4b10571b7ce3252fd0f0d1ba935e54c28f47c65f
Jacob Larsen <jac@kb.dk>
2014-09-20 21:18:07 +0200

    a simple entrance to the links

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>HACK4DK</title>
+	</head>
+	<body>
+		<h1>JAC4DK</h1>
+			<ul>
+				<li><a href="http://localhost:8888/hack4dk/search.php?from=1977-05-06&to=1977-05-06&format=json">Søg Json</a></li>
+				<li><a href="http://localhost:8888/hack4dk/object.php?guid=80f4e0a2-1dfb-af48-b12f-629c23eb99fc&format=xml">Hent XML (fuldtekst ikke tilgængelig via json)</a></li>
+			</ul>
+	</body>
+</html>
