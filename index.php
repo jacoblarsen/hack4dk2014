e18a701c6c3b84a72c69cf77bd42d02c2774ee19
Jacob Larsen <jac@kb.dk>
2014-09-20 23:00:04 +0200

    added link to documentation

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<body>
 		<h1>JAC4DK</h1>
 			<ul>
+				<li><a href="http://www.danskkulturarv.dk/programoversigter/">Dokumetation</a></li>
 				<li><a href="http://localhost:8888/hack4dk/search.php?from=1977-05-06&to=1977-05-06&format=json">Søg Json</a></li>
 				<li><a href="http://localhost:8888/hack4dk/object.php?guid=80f4e0a2-1dfb-af48-b12f-629c23eb99fc&format=xml">Hent XML (fuldtekst ikke tilgængelig via json)</a></li>
 			</ul>
