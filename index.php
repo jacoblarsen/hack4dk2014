faf60f4d3b49fd54f9b2e1e482ff2f45197ae78f
Jacob Larsen <jac@kb.dk>
2014-09-21 09:55:59 +0200

    FE: now pulls fulltext and displays in <pre> tags

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
-		<title>HACK4DK</title>
+		<title>JAC4DK</title>
 	</head>
 	<body>
 		<h1>JAC4DK</h1>
@@ -19,16 +19,24 @@
 			<p>
 				<div id="pdfplaceholder" ></div>
 			</p>	
-
 			<p>
 				<iframe src="" width="600" height="500" alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html" id="pdfembed" ></iframe>
 			</p>
+			<p>
+			
+				<div id="textplaceholder" ></div>
+			</p>
+
+			
+
 
 
   		<script src="//code.jquery.com/jquery-1.10.2.js"></script>	
   		<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
   		<script>
   			$(function() {
+    			
+    			// Add a datepicker
     			$( "#datepicker" ).datepicker({ dateFormat: "yy-mm-dd", 
     											defaultDate: "1977-05-05", 
     											changeMonth: true,
@@ -37,6 +45,7 @@
       											maxDate: "1983-12-31"
     										});
 
+  				// Get the pdf of the day
   				$( "#getit" ).on('click', function() {
   					$( "#pdfplaceholder").html("");
   					var date = $( "#datepicker" ).val();
@@ -46,26 +55,36 @@
   					$.ajax({
   						url: "search.php?from="+date+"&to="+date+"&format=json",
   						type: "json"
-  						}).done(function(data) 
-						{
-  							$.each(data.ModuleResults[0].Results, function(item,val){
-  								var type = val.Type;
-  								var url = val.Url;
-  								$( "#pdfplaceholder").append("<a href='" + url+"' target='_blank'>"+type+"</a><br/>");
+  					}).done(function(data) 
+					{
+  						$.each(data.ModuleResults[0].Results, function(item,val){
+  							var type = val.Type;
+  							var url = val.Url;
+  							var guid = val.Id;
+  							// If there are multiple pdf's. Link to them all
+  							$( "#pdfplaceholder").append("<a href='" + url+"' target='_blank'>"+type+"</a><br/>");
+	  						// IFrame the first pdf
+	  						// Only 'Schedule' since 'ScheduleNotes are borring'
+	  						if (type === 'Schedule'){
+	  							$("#pdfembed").attr("src",url);
 	  							
-	  							if (type === 'Schedule'){
-	  								$("#pdfembed").attr("src",url);
-	  							}
-
-	  						});
-	  						
-
-						});
-
+	  							$.ajax({
+  									url: "object.php?guid="+guid+"&format=xml",
+  									type: "xml"
+  								}).done(function(xml) 
+								{
+									var text = $(xml).find("MetadataXml").text()
+										.replace(/^(.*)CDATA./g,'').replace(/.....AllText(.*)$/g,'');
+  									$("#textplaceholder").html("<pre>" + text + "</pre>");
+  						
+				  				});
+	  						}
+	  					});
+	  				});
   				});
+
+  				
   			});
-
-
   		</script>
 	</body>
 </html>
